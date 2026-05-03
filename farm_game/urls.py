3b13b766721961159c3aff50ce13da7ca160b945
mobbyTech-dotcom <mrlxnlypx@gmail.com>
2026-05-03 19:30:03 +0700

    Add new API endpoints for plant and sync

--- farm_game/urls.py
+++ farm_game/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import GachaView
+from .views import GachaView, PlantView, SyncFarmView
 
 urlpatterns = [
     path('api/gacha/', GachaView.as_view(), name='gacha_api'),
+    path('api/plant/', PlantView.as_view(), name='plant_api'),
+    path('api/sync/', SyncFarmView.as_view(), name='sync_api'),
 ]
