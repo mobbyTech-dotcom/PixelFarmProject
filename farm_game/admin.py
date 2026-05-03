bd6beb25d68aa034e03f8a724e6911b26319a87a
mobbyTech-dotcom <mrlxnlypx@gmail.com>
2026-05-03 19:30:18 +0700

    Register PlayerProfile and FarmPlot models in admin

--- farm_game/admin.py
+++ farm_game/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import PlayerProfile, FarmPlot
 
-# Register your models here.
+@admin.register(PlayerProfile)
+class PlayerProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'money', 'bot_expire_time')
+
+@admin.register(FarmPlot)
+class FarmPlotAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'plot_index', 'plant_type', 'is_dead')
+    list_filter = ('is_dead', 'plant_type')
